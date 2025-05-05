3e12960a926adf97879e2e6e8707d539d26ab3ba
Sarvar Omonov <sarvaromon33@gmail.com>
2025-05-05 20:19:58 +0500

    Start Sereno app. Models.py and admin is written by me. In models.py added : User, Profile, Message, Attachment, Notificatioin, FriendRequest

---
+++ chat/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import User, Message, Profile, Attachment, Notification, FriendRequest
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active', 'is_staff', 'date_joined' )
+    search_fields = ('username', 'email')
+    
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'age', 'bio')
+    search_fields = ('user__username')
+    
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'timestamp', 'content')
+    search_fields = ('sender__username', 'content')
+    list_filter = ('timestamp')
+    
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('message', 'file_type', 'uploaded_at')
+    search_fields = ('message__content', 'file_type')
+    
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'is_read', 'created_at')
+    list_filter = ('is_read')
+    
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'created_at')
+    search_fields = ('from_user__username', 'to_user_username')
+    
+    
---
+++ chat/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)  # Kim yozganini saqlaymiz
+    content = models.TextField()                 # Xabar matni
+    timestamp = models.DateTimeField(default=timezone.now)  # Vaqti
+
+    def __str__(self):
+        return f'{self.sender.username}: {self.content[:20]}'
+
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    image = models.ImageField(upload_to='profile_pic/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True) # Optional text for bio
+    
+    def __str__(self):
+        return self.user.username
+    
+class Attachment(models.Model):
+    message = models.ForeignKey(Message, related_name='attachments', on_delete=models.CASCADE) # Xabarga bog'langan fayl
+    file = models.FileField(upload_to='attachments/') # Faylni yuklash joyi
+    uploaded_at = models.DateTimeField(auto_now_add=True) # Yuklangan vaqt
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE) # Kim yuklaganini saqlaymiz
+    
+    def __str__(self):
+        return f'Attachment by {self.added_by.username} at {self.uploaded_at}'
+    
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Kimga bildirishnoma 
+    message = models.ForeignKey(Message, on_delete=models.CASCADE) # Qaysi xabarga bog'langan
+    is_read = models.BooleanField(default=False) # O'qilganmi yoki yo'qmi 
+    created_at = models.DateTimeField(auto_now_add=True) # Qachon yaratilgan
+    
+    def __str__(self):
+        return f'Notification for {self.user.username} - Read: {self.is_read}'
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name='friend_requests_sent', on_delete=models.CASCADE) # Kimdan so'rov kelgan
+    to_user = models.ForeignKey(User, related_name='friend_requests_received', on_delete=models.CASCADE) # Kimga so'rov kelgan
+    created_at = models.DateTimeField(auto_now_add=True) # Qachon so'rov yuborilgan 
+    
+    def __str__(self):
+        return f'{self.from_user.username} -> {self.to_user.username}'
+    
